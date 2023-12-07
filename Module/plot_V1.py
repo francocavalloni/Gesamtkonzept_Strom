b5d4e0f0827bc3ceabdfcf5b8ad83eb9cecb0891
FrancoCavalloni <franco.cavalloni@fhnw.ch>
2023-12-07 15:26:53 +0100

    Update Excel parameters 2

--- Module/plot_V1.py
+++ Module/plot_V1.py
@@ -61,7 +61,8 @@ def plt_elkw(ELKW_Sim,INFO_ELKW,data_S):
             ax2.legend_.remove()
             fig.legend(loc="upper right", bbox_to_anchor=(1, 1), bbox_transform=ax2.transAxes)
             plt.title('Auswirkung E-LKW' + str(elkw))
-    return plt.show(), elkw
+            plt.show()
+    return plt.show()
 
 def plt_welkw(WELKW_Sim,INFO_WELKW,data_S):
     for welkw in range(1, WELKW_Sim + 1):
@@ -86,10 +87,10 @@ def plt_welkw(WELKW_Sim,INFO_WELKW,data_S):
                          label="Rücksp best.")
             sns.lineplot(x="Time", y="Last_WELKW" + str(welkw), data=subset_lkw, ax=ax1, linewidth=2.5,
                          color='green',
-                         label="Last WElkw")
+                         label="Last WELKW")
             sns.lineplot(x="Time", y="Ruecksp_WELKW" + str(welkw), data=subset_lkw, ax=ax1, linewidth=2.5,
                          color='yellowgreen',
-                         label="Rücksp WElkw")
+                         label="Rücksp WELKW")
             ax1.set_ylim(0, 1000)
             ax1.set_ylabel('Lastgang [kW]')
 
@@ -117,34 +118,47 @@ def plt_welkw(WELKW_Sim,INFO_WELKW,data_S):
             ax2.legend_.remove()
             fig.legend(loc="upper right", bbox_to_anchor=(1, 1), bbox_transform=ax2.transAxes)
             plt.title('Auswirkung WE-LKW' + str(welkw))
-    return plt.show(), welkw
+    return plt.show()
 
-def plt_epkw(EPKW_Sim,elkw,INFO_EPKW,data_S,ELKW_Sim,INFO_ELKW):
+def plt_epkw(INFO_EPKW,data_S,ELKW_Sim,INFO_ELKW,WELKW_Sim, INFO_WELKW):
     elkw = ELKW_Sim
+    welkw = WELKW_Sim
     time_windows = [('2022-05-23 00:00:00', '2022-05-30 00:00:00'),
                     ('2022-11-14 00:00:00', '2022-11-21 00:00:00')]
     for start_time, end_time in time_windows:
         subset_epkw = INFO_EPKW.loc[(INFO_EPKW['Time'] >= start_time) & (INFO_EPKW['Time'] <= end_time)]
-        if ELKW_Sim == 0:
-            subset_lg = data_S.loc[(INFO_EPKW['Time'] >= start_time) & (INFO_EPKW['Time'] <= end_time)]
-        else:
+
+        if ELKW_Sim >=1:
             subset_lkw = INFO_ELKW.loc[(INFO_ELKW['Time'] >= start_time) & (INFO_ELKW['Time'] <= end_time)]
+        elif WELKW_Sim >=1:
+            subset_lkw = INFO_WELKW.loc[(INFO_WELKW['Time'] >= start_time) & (INFO_WELKW['Time'] <= end_time)]
+        else:
+            subset_lg = data_S.loc[(INFO_EPKW['Time'] >= start_time) & (INFO_EPKW['Time'] <= end_time)]
+
         sns.set(rc={'figure.dpi': 300})
         sns.set_style("whitegrid")
         fig, ax1 = plt.subplots(figsize=(10, 6))
 
-        if ELKW_Sim == 0:
-            sns.lineplot(x="Time", y="kW_PV_S", data=subset_lg, ax=ax1, linewidth=2.5, color='orange',
-                         label="Last ohne EPKW")
-            sns.lineplot(x="Time", y="kW_RS_S", data=subset_lg, ax=ax1, linewidth=2.5, color='gold',
-                         label="Rücksp ohne EPKW")
-        else:
+        if ELKW_Sim >=1:
             sns.lineplot(x="Time", y="Last_ELKW" + str(elkw), data=subset_lkw, ax=ax1, linewidth=2.5,
                          color='orange',
                          label="Last ohne EPKW")
             sns.lineplot(x="Time", y="Ruecksp_ELKW" + str(elkw), data=subset_lkw, ax=ax1, linewidth=2.5,
                          color='gold',
                          label="Rücksp ohne EPKW")
+        elif WELKW_Sim >=1:
+            sns.lineplot(x="Time", y="Last_WELKW" + str(welkw), data=subset_lkw, ax=ax1, linewidth=2.5,
+                         color='orange',
+                         label="Last ohne EPKW")
+            sns.lineplot(x="Time", y="Ruecksp_WELKW" + str(welkw), data=subset_lkw, ax=ax1, linewidth=2.5,
+                         color='gold',
+                         label="Rücksp ohne EPKW")
+        else:
+            sns.lineplot(x="Time", y="kW_PV_S", data=subset_lg, ax=ax1, linewidth=2.5, color='orange',
+                         label="Last ohne EPKW")
+            sns.lineplot(x="Time", y="kW_RS_S", data=subset_lg, ax=ax1, linewidth=2.5, color='gold',
+                         label="Rücksp ohne EPKW")
+
         sns.lineplot(x="Time", y="Last_EPKW", data=subset_epkw, ax=ax1, linewidth=2.5, color='green',
                      label="Last mit EPKW")
         sns.lineplot(x="Time", y="Ruecksp_EPKW", data=subset_epkw, ax=ax1, linewidth=2.5, color='yellowgreen',
@@ -205,7 +219,17 @@ def plt_BS(Speicher,PROFIL_SPEICHER,INFO_SPEICHER):
                 sns.lineplot(x="Time", y="Ruecksp_" + str(S), data=subset_Speicher, ax=ax1, linewidth=2.5,
                              color='yellowgreen',
                              label="Rücksp mit Speicher")
-                ax1.set_ylim(0, 1000)
+                max_y_ax = subset_Speicher["Last_0"].max()
+                if max_y_ax < 500:
+                    y_ax_limit = 500
+                elif max_y_ax < 1000:
+                    y_ax_limit = 1000
+                elif max_y_ax < 1500:
+                    y_ax_limit = 1500
+                else:
+                    y_ax_limit = 2000
+
+                ax1.set_ylim(0, y_ax_limit)
                 ax1.set_ylabel('Lastgang [kW]')
 
                 ax2 = ax1.twinx()
